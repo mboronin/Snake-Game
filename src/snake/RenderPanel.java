9e87a7797a91886444da8dcb077da7364d956c7e
Mikhail Boronin <boroninm@gmail.com>
2020-04-07 00:00:06 +0200

    Game in progress

--- src/snake/RenderPanel.java
+++ src/snake/RenderPanel.java
@@ -1,14 +1,19 @@
 package snake;
+
 import javax.swing.*;
 import java.awt.*;
 
-
 public class RenderPanel extends JPanel {
+
+    public static int curColor = 0;
 
     @Override
     protected void paintComponent(Graphics g) {
-        g.setColor(Color.BLACK);
-        g.fillRect(0,0,300,700);
         super.paintComponent(g);
+        g.setColor(new Color(curColor));
+        g.fillRect(0, 0, 300, 700);
+        curColor++;
     }
+
 }
+
--- src/snake/Snake.java
+++ src/snake/Snake.java
@@ -2,22 +2,70 @@ package snake;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
-public class Snake {
+public class Snake implements ActionListener {
 
     public JFrame jframe;
+    public static Snake snake;
+    public RenderPanel renderPanel;
+    public Timer timer = new Timer(20, this);
+
+    public ArrayList<Point> snakeParts = new ArrayList<Point>();
+
+    public static final int UP = 0, DOWN = 1, LEFT = 2, RIGHT = 3, SCALE = 10;
+    public int ticks = 0, direction = DOWN;
+    Dimension dim;
+
+    public Point head, cherry;
 
     public Snake() {
-        Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+        dim = Toolkit.getDefaultToolkit().getScreenSize();
         jframe = new JFrame("Snake");
         jframe.setVisible(true);
         jframe.setSize(500, 500);
         //TODO Don't call fields of a class
         jframe.setLocation(dim.width / 2 - jframe.getWidth() / 2, dim.height / 2 - jframe.getWidth() - 2);
+        jframe.add(renderPanel = new RenderPanel());
         jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        head = new Point(0,0);
+        cherry = new Point(dim.width / SCALE, dim.height);
+        timer.start();
+    }
+
+    public void updateSnake(){
+
     }
 
     public static void main(String[] args) {
         Snake snake = new Snake();
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        renderPanel.repaint();
+        ticks++;
+        if (ticks % 10 == 0 && head != null){
+            if (direction == UP){
+                snakeParts.add(new Point(head.x, head.y - 1));
+            }
+            if (direction == DOWN){
+                snakeParts.add(new Point(head.x, head.y + 1));
+            }
+            if (direction == LEFT){
+                snakeParts.add(new Point(head.x - 1, head.y));
+            }
+            if (direction == RIGHT){
+                snakeParts.add(new Point(head.x + 1, head.y));
+            }
+            if (cherry != null){
+                if(head.x == cherry.x && head.y == cherry.y){
+                    score ++;
+                    cherry.setLocation(dim.width / SCALE, dim);
+                }
+            }
+        }
     }
 }
